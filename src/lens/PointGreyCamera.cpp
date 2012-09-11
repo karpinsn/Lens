6723735f0c68e53ca77b0e47abfe55a4b5d21b2c
Nik Karpinsky <karpinsn@iastate.edu>
2012-09-11 18:04:45 -0500

    Have PointGrayCamera init working. Need to work on the open function

--- src/lens/PointGreyCamera.cpp
+++ src/lens/PointGreyCamera.cpp
@@ -1,174 +1,96 @@
 /*
- *  JAICamera.cpp
+ *  PointGreyCamera.cpp
  *
  *  Created by Nikolaus Karpinsky on 9/20/10.
- *  Copyright 2010 ISU. All rights reserved.
+ *  Copyright 2012 ISU. All rights reserved.
  *
  */
 
-#include "JAICamera.h"
+#include "PointGreyCamera.h"
 
-lens::JAICamera::JAICamera(void)
+lens::PointGreyCamera::PointGreyCamera(void) : QThread()
 {
-	m_cameraImage = NULL;
+	m_convertedImage = NULL;
 }
 
-void lens::JAICamera::init(void)
+void lens::PointGreyCamera::init(void)
 {
-	_openFactory();
-	_openCamera();
-}
-
-void lens::JAICamera::open(void)
-{
-	J_STATUS_TYPE	retval = J_ST_SUCCESS;
-	int64_t int64Val;
-	SIZE	ViewSize;
-	int64_t pixelFormat;
+	FlyCapture2::Error error;
 	
-	// Get Width from the camera
-    retval = J_Camera_GetValueInt64(m_camera, NODE_NAME_WIDTH, &int64Val);
-    ViewSize.cx = (LONG)int64Val;     // Set window size cx
-
-    // Get Height from the camera
-    retval = J_Camera_GetValueInt64(m_camera, NODE_NAME_HEIGHT, &int64Val);
-    ViewSize.cy = (LONG)int64Val;     // Set window size cy
-
-    // Get pixelformat from the camera
-    retval = J_Camera_GetValueInt64(m_camera, NODE_NAME_PIXELFORMAT, &int64Val);
-    pixelFormat = int64Val;
-
-    // Calculate number of bits (not bytes) per pixel using macro
-    int bpp = J_BitsPerPixel(pixelFormat);
-
-	// Open stream
-    retval = J_Image_OpenStream(m_camera, 0, reinterpret_cast<J_IMG_CALLBACK_OBJECT>(this), reinterpret_cast<J_IMG_CALLBACK_FUNCTION>(&lens::JAICamera::streamCallBack), &m_thread, (ViewSize.cx*ViewSize.cy*bpp)/8);
-    if (retval != J_ST_SUCCESS) 
+	unsigned int numCameras;
+	error = m_busManager.GetNumOfCameras(&numCameras);
+	if(error != FlyCapture2::PGRERROR_OK)
 	{
-        //	Could not open the stream
-        return;
-    }
+		//	Could not get the number of attached cameras
+		return;
+	}
 
-    // Start Acquision
-    retval = J_Camera_ExecuteCommand(m_camera, NODE_NAME_ACQSTART);
-}
-
-void lens::JAICamera::close(void)
-{
-	_closeCamera();
-}
-
-float lens::JAICamera::getWidth(void)
-{
-	int64_t width;
-	J_Camera_GetValueInt64(m_camera, NODE_NAME_WIDTH, &width);
-	return (float)width;
-}
-
-float lens::JAICamera::getHeight(void)
-{
-	int64_t height;
-	J_Camera_GetValueInt64(m_camera, NODE_NAME_HEIGHT, &height);
-	return (float)height;
-}
-
-//--------------------------------------------------------------------------------------------------
-// StreamCBFunc
-//--------------------------------------------------------------------------------------------------
-void lens::JAICamera::streamCallBack(J_tIMAGE_INFO * pAqImageInfo)
-{
-    // We only want to create the OpenCV Image object once and we want to get the correct size from the Acquisition Info structure
-    if (m_cameraImage == NULL)
-    {
-        // Create the Image:
-        // We assume this is a 8-bit monochrome image in this sample
-        m_cameraImage = cvCreateImage(cvSize(pAqImageInfo->iSizeX,pAqImageInfo->iSizeY), IPL_DEPTH_8U, 1);
-    }
-
-	// Copy the data from the Acquisition engine image buffer into the OpenCV Image obejct
-    memcpy(m_cameraImage->imageData, pAqImageInfo->pImageBuffer, m_cameraImage->imageSize);
-
-	notifyObservers(m_cameraImage);
-}
-
-std::string lens::JAICamera::cameraName(void)
-{
-	return "JAI Camera Driver";
-}
-
-void lens::JAICamera::_openFactory(void)
-{
-	J_STATUS_TYPE status = J_Factory_Open("", &m_factory);
-	if(status != J_ST_SUCCESS) 
+	if(numCameras < 1)
 	{
-		//	Unable to open the factory. Show an error
+		//	No Point Grey cameras detected. 
+		return;
+	}
+
+	error = m_busManager.GetCameraFromIndex(0, &m_cameraGUID);
+	if(error != FlyCapture2::PGRERROR_OK)
+	{
+		return;
 	}
 }
-void lens::JAICamera::_openCamera(void)
+
+void lens::PointGreyCamera::open(void)
 {
-	J_STATUS_TYPE	status = J_ST_SUCCESS;
-	bool8_t			hasChanged = false;
-	uint32_t        tSize;
-
-	// Update camera list
-    status = J_Factory_UpdateCameraList(m_factory, &hasChanged);
-    if (status != J_ST_SUCCESS)
-    {
-		//	Could not update the camera list
-		return;
-    }
-
-	uint32_t        cameraCount;
-    // Get the number of Cameras
-    status = J_Factory_GetNumOfCameras(m_factory, &cameraCount);
-    if (status != J_ST_SUCCESS)
-    {
-        // Could not determine the number of cameras
-        return;
-    }
-    if (cameraCount == 0)
-    {
-        //	There is no camera
-        return;
-    }
-
-    // Get camera ID
-    tSize = (uint32_t)sizeof(m_cameraID);
-    status = J_Factory_GetCameraIDByIndex(m_factory, 0, m_cameraID, &tSize);
-    if (status != J_ST_SUCCESS)
-    {
-        //	Could not get a camera ID
-        return;
-    }
-
-    // Open camera
-    status = J_Camera_Open(m_factory, m_cameraID, &m_camera);
-    if (status != J_ST_SUCCESS)
-    {
-        //	Could not open the camera
-        return;
-    }
-    
-	//	Camera opened
-}
-
-void lens::JAICamera::_closeFactory(void)
-{
-}
-
-void lens::JAICamera::_closeCamera(void)
-{
-	if (m_camera)
+	FlyCapture2::Error error;
+	m_camera.Connect();
+	if(error != FlyCapture2::PGRERROR_OK)
 	{
-		// close camera
-		J_STATUS_TYPE status = J_Camera_Close(m_camera);
-		if(status != J_ST_SUCCESS) 
-		{
-			//	Show some error message
-		}
-		else
-		{
-			m_camera = NULL;
-		}
+		return;
+	}
+
+	FlyCapture2::CameraInfo camInfo;
+	error = m_camera.GetCameraInfo(&camInfo);
+	if(error != FlyCapture2::PGRERROR_OK)
+	{
+		return;
+	}
+
+	error = m_camera.StartCapture();
+	this->start();
+}
+
+void lens::PointGreyCamera::close(void)
+{
+	m_running = false;
+	FlyCapture2::Error error = m_camera.StopCapture();
+
+	if( error != FlyCapture2::PGRERROR_OK )
+    {
+		//	Failed to stop the camera
+		return;
+	}
+}
+
+float lens::PointGreyCamera::getWidth(void)
+{
+	return 1280.0f;
+}
+
+float lens::PointGreyCamera::getHeight(void)
+{
+	return 1024.0f;
+}
+
+std::string lens::PointGreyCamera::cameraName(void)
+{
+	return "Point Grey Camera Driver";
+}
+
+void lens::PointGreyCamera::run()
+{
+	FlyCapture2::Error error;
+	while(m_running)
+	{
+		error = m_camera.RetrieveBuffer( &m_rawImage );
+		//notifyObservers(m_convertedImage);
 	}
 }
--- src/lens/PointGreyCamera.h
+++ src/lens/PointGreyCamera.h
@@ -1,20 +1,20 @@
 /*
- Filename:		JAICamera.h
+ Filename:		PointGreyCamera.h
  Author:		Nikolaus Karpinsky
- Date Created:	09/20/10
- Last Edited:	09/20/10
+ Date Created:	09/11/12
+ Last Edited:	09/11/12
  
  Revision Log:
- 09/20/10 - Nik Karpinsky - Original creation.
+ 09/11/12 - Nik Karpinsky - Original creation.
  */
 
-#ifndef _JAI_CAMERA_H_
-#define _JAI_CAMERA_H_
-#define USE_JAI_CAMERA
+#ifndef _POINT_GREY_CAMERA_H_
+#define _POINT_GREY_CAMERA_H_
+#define USE_POINT_GREY_CAMERA
 
+#include <QThread>
+#include <FlyCapture2.h>
 #include "Camera.h"
-#include <Jai_Factory.h>
-//#include <stdint.h>
 
 #define NODE_NAME_WIDTH         "Width"
 #define NODE_NAME_HEIGHT        "Height"
@@ -25,33 +25,30 @@ using namespace std;
 
 namespace lens
 {
-    class JAICamera : public Camera
+    class PointGreyCamera : public Camera, QThread
 	{
 	private:
-		FACTORY_HANDLE	m_factory; // Factory Handle
-		CAM_HANDLE		m_camera; // Camera Handle
-		THRD_HANDLE     m_thread;      // Acquisition Thread Handle
-		int8_t          m_cameraID[J_CAMERA_ID_SIZE];    // Camera ID string
-		IplImage*		m_cameraImage;
+		FlyCapture2::Camera			m_camera; // Camera Handle
+		FlyCapture2::PGRGuid		m_cameraGUID; // Camera GUID
+		FlyCapture2::BusManager		m_busManager;
+		bool						m_running;
+		FlyCapture2::Image			m_rawImage;
+		IplImage*					m_convertedImage;
+		
 
     public:
-	  JAICamera(void);
-      virtual void init(void);
-      virtual void open(void);
-      virtual void close(void);
-	  virtual float getWidth(void);
-      virtual float getHeight(void);
+		PointGreyCamera(void);
+		virtual void init(void);
+		virtual void open(void);
+		virtual void close(void);
+		virtual float getWidth(void);
+		virtual float getHeight(void);
 
-      static std::string cameraName(void);
-	  void streamCallBack(J_tIMAGE_INFO * pAqImageInfo);
+		static std::string cameraName(void);
 
-	private:
-		void _openFactory(void);
-		void _openCamera(void);
-
-		void _closeFactory(void);
-		void _closeCamera(void);
+	protected:
+		void run();
 	};
 }
 
-#endif	// _JAI_CAMERA_H_
+#endif	// _POINT_GREY_CAMERA_H_
